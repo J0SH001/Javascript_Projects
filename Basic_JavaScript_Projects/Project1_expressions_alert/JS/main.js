dcdf20e1f9ffd09f5150d12a25c6642b024483f9
J0SH001 <gysljosh@gmail.com>
2023-07-15 13:25:22 -0500

    Basic fun with JavaScript

---
+++ Basic_JavaScript_Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,5 @@
+
+var l = '\'Hello, World!\' ' + ' Goodbye', k = "KARL!", j='Josh', Sent1 = "This is the beginning of the string", Sent2 = " and this is the end of the string"; //defining variables l, k, Sent1, Sent2 and j
+document.write(Sent1 + Sent2); //concatenate variables Sent1 and Sent2 on the screen
+window.alert(l); //make a window alert that displays variable l
+document.write(" 3 + 3") //display an expression
