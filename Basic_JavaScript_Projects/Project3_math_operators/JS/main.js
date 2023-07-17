0a9e7b5d2c89f91dd9c697d409d706b0af766c69
J0SH001 <gysljosh@gmail.com>
2023-07-17 18:17:11 -0500

    Comments

--- Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
+++ Basic_JavaScript_Projects/Project3_math_operators/JS/main.js
@@ -1,33 +1,33 @@
-function alex() {
-    var lex = 5 * (3 + 2);
-    document.getElementById("bert").innerHTML = lex;
+function alex() { //Declaring the function
+    var lex = 5 * (3 + 2); //Defining the variable
+    document.getElementById("bert").innerHTML = lex; // PEMDAS example, linking to HTML element
 }
 
-function alexander() {
-    var lexi = 9 / 3;
-    document.getElementById("echbert").innerHTML = lexi;
+function alexander() { //Declaring the function
+    var lexi = 9 / 3;//Defining the variable
+    document.getElementById("echbert").innerHTML = lexi; //Division example, linking to HTML element
 }
 
-function alexa() {
-    var lexicon = 8 % 3;
-    document.getElementById("albert").innerHTML = lexicon;
+function alexa() { //Declaring the function
+    var lexicon = 8 % 3; //Defining the variable
+    document.getElementById("albert").innerHTML = lexicon; //Remainder example, linking to HTML element
 }
 
-function alexandria() {
-    var lexluther = 9 / 3;
-    document.getElementById("bert2").innerHTML = -lexluther;
+function alexandria() { //Declaring the function
+    var lexluther = 9 / 3; //Defining the variable
+    document.getElementById("bert2").innerHTML = -lexluther; //Division inverse example, linking to HTML element
 }
 
-function dope() {
-    var teddy = 5;
-    teddy++;
-    document.getElementById("1").innerHTML = teddy;
+function dope() { //Declaring the function
+    var teddy = 5; //Defining the variable
+    teddy++; //Same as add 1, increment
+    document.getElementById("1").innerHTML = teddy; //Increment example, linking to HTML element
 }
 
-function dopey() {
-    var teddy = 5;
-    teddy--;
-    document.getElementById("2").innerHTML = teddy;
+function dopey() { //Declaring the function
+    var teddy = 5; //Defining the variable
+    teddy--; //Same as subtract 1, decrement
+    document.getElementById("2").innerHTML = teddy; //Decrement example, linking to HTML element
 }
 
-window.alert(Math.random());
+window.alert(Math.random()); //Setting up an alert pop up and displaying a random number between 0 & 1
