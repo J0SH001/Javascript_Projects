dc2f2afa3d0f38ed8bc50c8ce92ee984e3e26e68
J0SH001 <gysljosh@gmail.com>
2023-07-24 17:16:43 -0500

    Corrections

--- Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
+++ Basic_JavaScript_Projects/Project6_ternary__operators_constructors/JS/main.js
@@ -2,7 +2,10 @@ function Ride_Function() { //Declaring a function
     var Height, Can_ride; //Declaring variables
     Height = document.getElementById("Height").value; //Retrieve user input
     Can_ride = (Height < 17) ? "You are too young":"You are old enough"; //Ternary operator based on whether or not the number is less than 17
-    function to_vote() {Can_ride += " to vote";} //Declaring a nested function
-    to_vote();  //Performing nested function
-    document.getElementById("Ride").innerHTML = Can_ride; //Displayed result
+    function things(){
+        function to_vote() {Can_ride += " to vote.";} //Declaring a nested function
+        to_vote();  //Performing nested function
+        return Can_ride
+    }
+    document.getElementById("Ride").innerHTML = things(); //Displayed result
 }
---
+++ Basic_JavaScript_Projects/Project7_scope_time_function/JS/main.js
@@ -0,0 +1,6 @@
+var x = 5;
+function simple_math(){
+    var y = 10;
+    document.write(x + y);
+}
+
