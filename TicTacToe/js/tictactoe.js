d725646d0d62c120c3aa678b0ee3642ce888ee46
J0SH001 <gysljosh@gmail.com>
2023-11-03 20:26:30 -0500

    Update tictactoe.js

--- TicTacToe/js/tictactoe.js
+++ TicTacToe/js/tictactoe.js
@@ -74,15 +74,15 @@ function checkWinConditions(){
     // X 3, 4, 5 condition.
     else if(arrayIncludes('3X', '4X', '5X')) {drawWinLine(50, 304, 558, 304)}
     // X 6, 7, 8 condition.
-    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 450, 558, 450)}
+    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50, 500, 558, 500)}
     // X 0, 3, 6 condition.
     else if(arrayIncludes('0X', '3X', '6X')) {drawWinLine(100, 50, 100, 558)}
     // X 1, 4, 7 condition.
     else if(arrayIncludes('1X', '4X', '7X')) {drawWinLine(304, 50, 304, 558)}
     // X 2, 5, 8 condition.
-    else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(450, 50, 450, 558)}
+    else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(500, 50, 500, 558)}
     // X 6, 4, 2 condition.
-    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 450, 510, 90)}
+    else if (arrayIncludes('6X', '4X', '2X')) { drawWinLine(100, 500, 510, 90)}
     // X 0, 4, 8 condition.
     else if(arrayIncludes('0X', '4X', '8X')) {drawWinLine(100, 100, 520, 520)}
     // O 0, 1, 2 condition.
@@ -90,15 +90,15 @@ function checkWinConditions(){
     // O 3, 4, 5 condition.
     else if(arrayIncludes('3O', '4O', '5O')) {drawWinLine(50, 304, 558, 304)}
     // O 6, 7, 8 condition.
-    else if(arrayIncludes('6O', '7O', '8O')) {drawWinLine(50, 450, 558, 450)}
+    else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50, 500, 558, 500)}
     // O 0, 3, 6 condition.
     else if(arrayIncludes('0O', '3O', '6O')) {drawWinLine(100, 50, 100, 558)}
     // O 1, 4, 7 condition.
     else if(arrayIncludes('1O', '4O', '7O')) {drawWinLine(304, 50, 304, 558)}
     // O 2, 5, 8 condition.
-    else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(450, 50, 450, 558)}
+    else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(500, 50, 500, 558)}
     // O 6, 4, 2 condition.
-    else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 450, 510, 90)}
+    else if (arrayIncludes('6O', '4O', '2O')) { drawWinLine(100, 500, 510, 90)}
     // O 0, 4, 8 condition.
     else if(arrayIncludes('0O', '4O', '8O')) {drawWinLine(100, 100, 520, 520)}
     //This condition checks for a tie. If none of the above conditions are met and
